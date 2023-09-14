24af9a578432190f2b16afa4e12765bb6905e01a
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-09-15 02:15:45 +0600

    Calculate total price

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Cart = ({ selectedCourse, creditHour, remainingHour }) => {
+  //   Calculate total price
+  let price = 0;
+  selectedCourse.map((course) => {
+    price = price + course.price;
+  });
+
   return (
     <div className="w-1/4 bg-white p-4 self-start rounded-lg flex flex-col gap-2">
       <h3 className="text-[#2F80ED] text-lg font-bold border-b-2 py-2">
@@ -17,7 +23,7 @@ const Cart = ({ selectedCourse, creditHour, remainingHour }) => {
       <h3 className="border-b-2 border-t-2 py-2 font-medium">
         Total Credit Hour: {creditHour}
       </h3>
-      <h3 className="font-semibold">Total Price: 100 USD</h3>
+      <h3 className="font-semibold">Total Price: {price.toFixed(2)} USD</h3>
     </div>
   );
 };
